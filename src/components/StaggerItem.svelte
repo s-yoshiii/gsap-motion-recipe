a4ef39256e03e2f8c851a1a2cbd49605cba668fe
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-30 00:26:34 +0900

    scssの追加、各セクションごとにcomponentを分ける

---
+++ src/components/StaggerItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+<section>
+    StaggerItem
+</section>
+<style lang="scss">
+    $color01:#FFD4D4;
+    $color02:#FFFFE8;
+    $color03:#CDE990;
+    $color04:#AACB73;
+    section{
+        background: $color02;
+    }
+</style>
