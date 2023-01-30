3efa1ba6e4de6640642fef5bd31efda4ff846a7e
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-30 12:17:07 +0900

    GSAサンプルStagger作成、ResetCSSの読み込み

---
+++ src/components/SectionLayout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let cl: string | null;
+</script>
+
+<section class={cl ? cl : ""}>
+  <div>
+    <slot props={cl} />
+  </div>
+</section>
+
+<style lang="scss">
+  $stagger: #ffffe8;
+  section {
+    padding: 70px 0;
+    & > div {
+      margin: 0 auto;
+      max-width: 1080px;
+      padding: 0 50px;
+    }
+  }
+  .stagger {
+    background: $stagger;
+  }
+</style>
--- src/components/StaggerItem.svelte
+++ src/components/StaggerItem.svelte
@@ -1,15 +1,65 @@
 <script lang="ts">
-
+  import { gsap } from "gsap";
+  import { onMount } from "svelte";
+  import SectionLayout from "./SectionLayout.svelte";
+  onMount(() => {
+    gsap.from("li", {
+      y: 10,
+      autoAlpha: 0,
+      duration: 1,
+      ease: "power4.out",
+      stagger: 0.1,
+    });
+  });
 </script>
-<section>
-    StaggerItem
-</section>
+
+<SectionLayout cl="stagger">
+  <h1>Stagger</h1>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+    <li>11</li>
+    <li>12</li>
+    <li>13</li>
+    <li>14</li>
+    <li>15</li>
+    <li>16</li>
+    <li>17</li>
+    <li>18</li>
+  </ul>
+</SectionLayout>
+
 <style lang="scss">
-    $color01:#FFD4D4;
-    $color02:#FFFFE8;
-    $color03:#CDE990;
-    $color04:#AACB73;
-    section{
+  $color01: #ffd4d4;
+  $color02: #cde990;
+  $color03: #aacb73;
+  ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 100px);
+    grid-auto-rows: 100px;
+    justify-content: center;
+    gap: 10px;
+    li {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &:nth-child(3n + 1) {
+        background: $color01;
+      }
+      &:nth-child(3n + 2) {
         background: $color02;
+      }
+      &:nth-child(3n) {
+        background: $color03;
+      }
     }
+  }
 </style>
