afd78d470c5e03c584fe90357b5c618f7ed21326
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-30 18:26:08 +0900

    スクロールトリガーを試す

---
+++ src/components/ScrollTriggerItem.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { gsap } from "gsap";
+  import { onMount } from "svelte";
+  import SectionLayout from "./SectionLayout.svelte";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+  gsap.registerPlugin(ScrollTrigger);
+  onMount(() => {
+    gsap.to(".box", {
+      x: "70vw",
+      rotation: 360,
+      duration: 3,
+
+      scrollTrigger: {
+        trigger: ".box",
+        start: "top bottom",
+        end: "bottom top",
+        toggleActions: "play none none reset",
+        markers: true,
+        id: "box",
+        //scrub: true,
+        scrub: 0.5, // smoothing
+      },
+    });
+  });
+</script>
+
+<SectionLayout cl="scroll">
+  <div class="box" />
+</SectionLayout>
+
+<style lang="scss">
+  .box {
+    width: 50px;
+    height: 50px;
+    background: #6fb936;
+    border-radius: 10px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-  import RadianItem from "../components/radianItem.svelte";
+  import RadianItem from "../components/RadianItem.svelte";
   import RandomStaggerItem from "../components/RandomStaggerItem.svelte";
+  import ScrollTriggerItem from "../components/ScrollTriggerItem.svelte";
   import StaggerItem from "../components/StaggerItem.svelte";
 </script>
 
 <StaggerItem />
 <RandomStaggerItem />
 <RadianItem />
+<ScrollTriggerItem />
 
 <style lang="scss">
 </style>
