3adb85d3a57e3d172ade5b1f12c135b717efb694
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-31 10:57:52 +0900

    ScrollTriggerで文字が1文字ずつ表示されるように設定

--- src/components/ScrollTriggerItem.svelte
+++ src/components/ScrollTriggerItem.svelte
@@ -4,7 +4,7 @@
   import SectionLayout from "./SectionLayout.svelte";
   import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
   gsap.registerPlugin(ScrollTrigger);
-  let contInner: HTMLDivElement;
+  let cont: HTMLDivElement;
 
   onMount(() => {
     // gsap.to(".box", {
@@ -21,8 +21,8 @@
     //     //scrub: true,
     //     scrub: 0.5, // smoothing
     //   },
-    // });8
-    const sentences = [...contInner.querySelectorAll("p")];
+    // });
+    const sentences = [...cont.querySelectorAll("p")];
     for (const item of sentences) {
       let output = "";
       const content = item.innerHTML.replace(/<br\s*\/?>/gi, "\n");
@@ -31,33 +31,117 @@
       }
       item.innerHTML = output;
     }
+    let q = gsap.utils.selector(cont);
+    let boxes = q(".cont__inner");
+    boxes.forEach((box) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: box,
+          start: "top bottom -=20",
+          toggleActions: "restart none none none", // onEnter, onLeave, onEnterBack, onLeaveBack
+          // markers: true,
+        },
+      });
+
+      tl.from(box, {
+        opacity: 0,
+        y: 20,
+      });
+      tl.from(
+        gsap.utils.toArray("span", box),
+        {
+          opacity: 0,
+          duration: 0.01,
+          stagger: 0.005,
+        },
+        "-=1"
+      );
+    });
   });
 </script>
 
 <SectionLayout cl="scroll">
   <h2>Scroll Trigger</h2>
-  <div class="cont">
-    <div class="cont__inner" bind:this={contInner}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
-        facilis quasi esse! Hic doloribus provident placeat a perferendis
-        consequatur soluta animi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
-        facilis quasi esse! Hic doloribus provident placeat a perferendis
-        consequatur soluta animi.
-      </p>
+  <div class="cont" bind:this={cont}>
+    <div class="cont__sect">
+      <div class="cont__inner">
+        <p>
+          aaaaaaaaaaaaaaaaaaaaLorem ipsum dolor sit amet consectetur adipisicing
+          elit. Maxime molestias, nam atque quam doloribus temporibus eaque
+          impedit! Suscipit facilis quasi esse! Hic doloribus provident placeat
+          a perferendis consequatur soluta animi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
+          facilis quasi esse! Hic doloribus provident placeat a perferendis
+          consequatur soluta animi.
+        </p>
+      </div>
+    </div>
+    <div class="cont__sect">
+      <div class="cont__inner">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
+          facilis quasi esse! Hic doloribus provident placeat a perferendis
+          consequatur soluta animi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
+          facilis quasi esse! Hic doloribus provident placeat a perferendis
+          consequatur soluta animi.
+        </p>
+      </div>
+    </div>
+    <div class="cont__sect">
+      <div class="cont__inner">
+        <p>
+          33333Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
+          facilis quasi esse! Hic doloribus provident placeat a perferendis
+          consequatur soluta animi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          molestias, nam atque quam doloribus temporibus eaque impedit! Suscipit
+          facilis quasi esse! Hic doloribus provident placeat a perferendis
+          consequatur soluta animi.
+        </p>
+      </div>
     </div>
   </div>
 </SectionLayout>
 
 <style lang="scss">
+  @use "../assets/variable";
   .cont {
+    $parent: &;
+    & + & {
+      margin-top: 100px;
+    }
+    &__sect {
+      display: flex;
+      & + & {
+        margin-top: 100px;
+      }
+      &:nth-of-type(2n) {
+        justify-content: flex-end;
+        #{$parent}__inner {
+          background: variable.$scroll-color02;
+        }
+      }
+      &:nth-of-type(3n) {
+        #{$parent}__inner {
+          background: variable.$scroll-color03;
+        }
+      }
+    }
     &__inner {
-      padding: 1rem;
+      padding: 2rem;
+      display: inline-block;
+      background: variable.$scroll-color01;
     }
     & p {
       font-size: 18px;
